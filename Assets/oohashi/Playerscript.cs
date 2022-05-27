5ae340e6a78fee788d495ba477b8b9ac569862ed
RK <riku.kuu0421@gmail.com>
2022-05-27 15:13:21 +0900

    無敵時間作成

--- Assets/oohashi/Playerscript.cs
+++ Assets/oohashi/Playerscript.cs
@@ -6,12 +6,17 @@ public class Playerscript : MonoBehaviour
 {
     [SerializeField] float horizontalSpeed;
     [SerializeField] int jumpForce;
+    [SerializeField] float emperorTime = 0;
     Rigidbody2D rb;
     int jumpcount = 0;
+   [SerializeField] bool _canhit = true;
+    CapsuleCollider2D collider;
+    float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent < Rigidbody2D >();
+        collider = GetComponent<CapsuleCollider2D>();
     }
 
     void FixedUpdate()
@@ -38,12 +43,28 @@ public class Playerscript : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);//ジャンプ計算
             jumpcount++;
         }
+        if(_canhit == false)
+        {
+            collider.enabled = false;
+            timer += Time.deltaTime;
+            if(emperorTime < timer)
+            {
+                _canhit = true;
+                collider.enabled = true;
+                timer = 0;
+            }
+        }
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "ground")
         {
             jumpcount = 0;//カウントリセット
+        }
+        if(collision.gameObject.tag == "enemy")
+        {
+            GameManager.zanki--;
+            _canhit = false;
         }
     }
 }
