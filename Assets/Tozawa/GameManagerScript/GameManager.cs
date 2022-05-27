a79250861661a4005f1922778f167d1f01adab8d
戸澤海智 <st37071556@icloud.com>
2022-05-27 16:56:51 +0900

    チェックポイントの処理の下書き

--- Assets/Tozawa/GameManagerScript/GameManager.cs
+++ Assets/Tozawa/GameManagerScript/GameManager.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject _spawnPoint;
+    Transform _sp = default;
+    [SerializeField] GameObject _checkPoint;
     [SerializeField] public static int zanki = 3;
-    
+    public static bool checkpointON = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(checkpointON==true)
+        {
+            _sp = _checkPoint.transform;//チェックポイント通過したらスポーンポイントをチェックポイントに変更
+        }
         if (zanki <= 0)
         {
             GameOver();
@@ -26,7 +31,8 @@ public class GameManager : MonoBehaviour
 
     void GameStart()//ゲーム開始とともにプレイヤーｯを生成しゲーム開始
     {
-
+        checkpointON = false;
+        _sp = _spawnPoint.transform;//初期スポーンポイント設定
     }
     void GameOver()//ゲームオーバーの条件（残機数）を満たしたらUIで負けを表示しタイトルをロードする
     {
