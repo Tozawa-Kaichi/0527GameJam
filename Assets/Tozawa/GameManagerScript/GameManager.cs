e9e2d8ff4648f630608c21089c101bc1799463eb
戸澤海智 <st37071556@icloud.com>
2022-05-27 16:12:35 +0900

    ファイルの位置間違えていたので移動

--- Assets/Tozawa/GameManagerScript/GameManager.cs
+++ Assets/Tozawa/GameManagerScript/GameManager.cs
@@ -5,21 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject _spawnPoint;
     [SerializeField] public static int zanki = 3;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameStart();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (zanki <= 0)
+        {
+            GameOver();
+        }
     }
 
-    void GameStart()
+    void GameStart()//ゲーム開始とともにプレイヤーｯを生成しゲーム開始
     {
 
     }
@@ -28,7 +33,10 @@ public class GameManager : MonoBehaviour
 
 
     }
+    void Clear()//ゴールに触れる・ボスを倒したらWinUIを表示して次のシーンに行く
+    {
 
+    }
 
     
 }
