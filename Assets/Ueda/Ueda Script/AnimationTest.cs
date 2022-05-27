913c2640662aee9f32fe0a29b7be10a433676b4c
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-05-27 17:58:07 +0900

    攻撃のアニメーションとアニメーションの動作確認用のスクリプトを追加

---
+++ Assets/Ueda/Ueda Script/AnimationTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalky = Input.GetAxisRaw("Horizontal");
+        if (horizontalky == 0)
+        {
+            anim.SetBool("isRun", false);
+        }
+        else
+        {
+            anim.SetBool("isRun", true);
+        }
+    }
+}
