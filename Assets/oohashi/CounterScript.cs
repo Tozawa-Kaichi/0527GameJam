0127d84dcb9b3521d373cb18f0f60c285fdf2d9d
RK <riku.kuu0421@gmail.com>
2022-05-28 14:17:27 +0900

    カウンター完成

--- Assets/oohashi/CounterScript.cs
+++ Assets/oohashi/CounterScript.cs
@@ -7,15 +7,20 @@ public class CounterScript : MonoBehaviour
     [SerializeField] float counterTime;
     [SerializeField] float skillHunter;//カウンター時の無敵時間
     [SerializeField] GameObject player;
+    [SerializeField] bool _cancounter = true;//後でserializefieldを消す
     bool counterbool = false;
     Playerscript muteki;
+    Collider2D atari;
     BoxCollider2D bc;
     float timer = 0;
+   // public CapsuleCollider2D collider;
     // Start is called before the first frame update
     void Start()
     {
         bc = GetComponent<BoxCollider2D>();
         muteki =player.GetComponent<Playerscript>();
+        atari = player.GetComponent<CapsuleCollider2D>();
+
     }
 
     // Update is called once per frame
@@ -34,14 +39,27 @@ public class CounterScript : MonoBehaviour
     }
     void Counter()
     {
-        timer += Time.deltaTime;
-        muteki.Muteki(skillHunter);
         bc.enabled = true;
+        timer += Time.deltaTime;
+        if (_cancounter == false)
+        {
+            Playerscript._canhit = false;
+            muteki.Muteki(skillHunter);
+            _cancounter = true;
+        }
+        
         if (counterTime < timer)
         {
             counterbool = false;
             bc.enabled = false;
             timer = 0;
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            _cancounter = false;
         }
     }
 }
