5680a3325987e88ca8f95e463c8796cde77d9d1e
Kaipon_0615 <kaipon0615creator@gmail.com>
2022-05-28 12:59:00 +0900

    ステージ分割

--- Assets/OgataKaito/NewBehaviourScript.cs
+++ Assets/OgataKaito/NewBehaviourScript.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    //[SerializeField] float m_speed
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
