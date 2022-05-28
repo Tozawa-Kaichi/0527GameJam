d23ef1203c7a4fa9b853b9bcb59361ecf7f6f33e
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-05-28 10:29:11 +0900

    アニメーションの調整

--- Assets/Ueda/Ueda Script/AnimationTest.cs
+++ Assets/Ueda/Ueda Script/AnimationTest.cs
@@ -15,7 +15,7 @@ public class AnimationTest : MonoBehaviour
     void Update()
     {
         float horizontalky = Input.GetAxisRaw("Horizontal");
-        if (horizontalky == 0)
+        if (horizontalky == 0.0)
         {
             anim.SetBool("isRun", false);
         }
@@ -23,5 +23,22 @@ public class AnimationTest : MonoBehaviour
         {
             anim.SetBool("isRun", true);
         }
+        bool jumpky = Input.GetButtonDown("Jump");
+        if (jumpky) //ÉWÉÉÉìÉvÇÃâÒêîêßå¿
+        {
+            anim.SetBool("isJump",true);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("isAttack");
+        }
     }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            anim.SetBool("isJump",false);
+        }
+    }
+    
 }
